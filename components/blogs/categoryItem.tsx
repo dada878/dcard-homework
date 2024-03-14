8ea59f2b06412959fae1ab206b9e1a226fc2cbf6
Dada878 <xx0932399@gmail.com>
2024-03-14 20:30:55 +0800

    fix wrong background color on category item component

--- components/blogs/categoryItem.tsx
+++ components/blogs/categoryItem.tsx
@@ -5,7 +5,7 @@ export default function CategoryItem({children, count} : {children: ReactNode, c
         <div className="flex justify-between text-secondary cursor-pointer hover:text-primary py-2 transition">
             {children}
             {/* TODO: make border color white when hove */}
-            <div className="bg-red flex-1 border-b border-b-secondary mx-2 -translate-y-1/2"></div>
+            <div className="flex-1 border-b border-b-secondary mx-2 -translate-y-1/2"></div>
             <p>{count} 篇</p>
         </div>
     );
