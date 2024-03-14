5605522676b85a2f08540a4c85be17af098524b7
Dada878 <xx0932399@gmail.com>
2024-03-14 20:23:21 +0800

    move css files to assets folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/assets/globals.css";
 import Navbar from "@/components/global/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
