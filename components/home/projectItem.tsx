a035c816499a152ee499c8b660f39a1275cef802
Dada878 <xx0932399@gmail.com>
2024-03-15 01:16:43 +0800

    add hover effect to project item component

--- components/home/projectItem.tsx
+++ components/home/projectItem.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 
-
 export default function ProjectItem({name, image} : {name : string, image: string}) {
   return (
-    <div className="bg-secondary rounded-lg flex flex-col gpa-2">
-      <div style={{width: '100%', height: '100%', position: 'relative'}}>
+        <div className="bg-secondary rounded-lg flex flex-col gpa-2 hover:scale-105 cursor-pointer transition">
+    <div style={{width: '100%', height: '100%', position: 'relative'}}>
         <Image
           alt='Mountains'
           src={image}
