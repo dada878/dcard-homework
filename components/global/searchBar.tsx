2ced01eaa1101244717c2d3c50959cee2d666aa8
Dada878 <xx0932399@gmail.com>
2024-03-14 20:37:11 +0800

    add cursor pointer class to search bar icon

--- components/global/searchBar.tsx
+++ components/global/searchBar.tsx
@@ -4,7 +4,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 export default function SearchBar() {
     return (
         <div className="bg-[#6C7693] bg-opacity-30 p-2 rounded-lg items-center flex gap-3 pl-3">
-            <FontAwesomeIcon icon={faSearch} />
+            <FontAwesomeIcon className="cursor-pointer" icon={faSearch} />
             <input type="text" placeholder="Search something..." className="d bg-opacity-0 bg-white outline-none" />
         </div>
     );
