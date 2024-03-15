79cdba98049364ba28b064f6225f5fe0dce35da2
Dada878 <xx0932399@gmail.com>
2024-03-16 07:57:19 +0800

    optimize RWD for container component

--- components/global/container.tsx
+++ components/global/container.tsx
@@ -9,7 +9,7 @@ export default function Container({
 }) {
   return (
     <main
-      className={`flex flex-col gap-5 ml-0 md:ml-64 lg:ml-80 p-10 max-w-screen lg:max-w-[min(calc(100vw_-_19rem),56rem)] w-auto lg:w-4/6 ${className}`}
+      className={`flex flex-col gap-5 ml-0 md:ml-64 lg:ml-80 p-6 md:p-10 max-w-screen lg:max-w-[min(calc(100vw_-_19rem),56rem)] w-auto lg:w-4/6 ${className}`}
     >
       {children}
     </main>
