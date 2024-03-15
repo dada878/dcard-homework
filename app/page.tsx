25cc7d23180d56a6007e85f034b3daebb6104b0f
Dada878 <xx0932399@gmail.com>
2024-03-15 23:05:14 +0800

    implemented RWD on home

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <HeroBanner />
-      <div className="text-start flex flex-col items-stretch gap-12 px-48 mb-10">
+      <div className="text-start flex flex-col items-stretch gap-12 md:px-32 lg:px-48 mb-10">
         <AboutSection title="關於">
           <p>嗨，我是一個熱衷於網頁前端開發的高中生，我喜歡不斷學習和成長，並追求在技術領域中不斷超越自我！</p>
         </AboutSection>
@@ -16,7 +16,7 @@ export default function Home() {
           <li>SCINT 北台灣學生資訊社群開發組</li>
         </AboutSection>
         <AboutSection title="作品集">
-          <div className="grid grid-cols-3 gap-7">
+          <div className="grid grid-col-1 md:grid-cols-3 gap-7">
             <ProjectItem name="番茄鐘" image="/images/projects/pomodoro-timer.png" />
             <ProjectItem name="鞋子の網站" image="/images/projects/shoes.png" />
             <ProjectItem name="SCINT 官網" image="/images/projects/scint.png" />
--- components/home/heroBanner.tsx
+++ components/home/heroBanner.tsx
@@ -3,8 +3,8 @@ import GlowEffect from "./glowEffect";
 export default function HeroBanner() {
   return (
     <header className="text-center h-[calc(100vh_-_6rem)] flex flex-col justify-center mb-12">
-      <h1 className="text-7xl font-black mb-5 z-10 blur-in">Dada&apos;s Blog</h1>
-      <p className="text-[#BBBBBB] z-10 text-xl blur-in">我會在這裡分享各種技術文章及日常生活中有趣的事物</p>
+      <h1 className="text-5xl md:text-7xl font-black mb-5 z-10 blur-in">Dada&apos;s Blog</h1>
+      <p className="text-[#BBBBBB] z-10 text-md md:text-xl blur-in">我會在這裡分享各種技術文章及生活中的的事物</p>
       <div className="flex justify-center">
         <GlowEffect />
       </div>
