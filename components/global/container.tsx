5cc1a31fd7b4423cb31bc0f51d48ab81c67c255f
Dada878 <xx0932399@gmail.com>
2024-03-16 07:45:12 +0800

    optimize RWD for container component and fixed sidebar component

--- components/global/container.tsx
+++ components/global/container.tsx
@@ -1,7 +1,17 @@
 import { ReactNode } from "react";
 
-export default function Container({ children }: { children: ReactNode }) {
+export default function Container({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
-    <main className="flex flex-col gap-5 ml-80 p-10 max-w-4xl w-4/6">{children}</main>
+    <main
+      className={`flex flex-col gap-5 ml-0 md:ml-64 lg:ml-80 p-10 max-w-screen lg:max-w-[min(calc(100vw_-_19rem),56rem)] w-auto lg:w-4/6 ${className}`}
+    >
+      {children}
+    </main>
   );
 }
--- components/global/fixedSidebar.tsx
+++ components/global/fixedSidebar.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
-export default function FixedSidebar({ children }: { children: ReactNode }) {
-  return <aside className="flex flex-col gap-5 w-60 fixed m-10">{children}</aside>;
+export default function FixedSidebar({ children, className }: { children: ReactNode, className?: string}) {
+  return <aside className={`hidden md:flex flex-col gap-5 w-60 fixed m-10 ${className}`}>{children}</aside>;
 }
