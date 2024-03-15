f4ea7aa809d8ca282cef87795367b86a5fdeb299
Dada878 <xx0932399@gmail.com>
2024-03-16 02:36:09 +0800

    optimize RWD for project item component

--- components/home/projectItem.tsx
+++ components/home/projectItem.tsx
@@ -8,18 +8,20 @@ export default function ProjectItem({
   image: string;
 }) {
   return (
-    <div className="bg-secondary rounded-lg flex flex-col gpa-2 cursor-pointer transition hover:scale-105">
-      <div className="w-full h-full relative">
-        <Image
-          alt="Mountains"
-          src={image}
-          className="rounded-t-lg"
-          width={500}
-          height={100}
-        />
-      </div>
-      <div className="bg-secondary rounded-b-lg text-center p-2">
-        <p>{name}</p>
+    <div className="flex justify-center">
+      <div className="bg-secondary rounded-lg flex flex-col gpa-2 cursor-pointer transition hover:scale-105 max-w-md">
+        <div className="w-full h-full relative flex justify-center">
+          <Image
+            alt="Mountains"
+            src={image}
+            className="rounded-t-lg"
+            width={500}
+            height={100}
+          />
+        </div>
+        <div className="bg-secondary rounded-b-lg text-center p-2">
+          <p>{name}</p>
+        </div>
       </div>
     </div>
   );
