434187392cd5933783036e1391c1f1522f6d6886
Dada878 <xx0932399@gmail.com>
2024-03-19 21:54:36 +0800

    redirect to blogs page after creating a post

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import PostEditor from "@/components/global/postEditor";
 import { Post } from "@/types/post";
+import { useRouter } from "next/navigation";
 
 export default function CreatePage() {
+  const router = useRouter();
   const createPostCallback = (post: Post) => {
     fetch("/api/create", {
       method: "POST",
       body: JSON.stringify(post),
+    }).then(() => {
+      router.push("/blogs");
     });
   };
   return (
