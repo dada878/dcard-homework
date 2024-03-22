029d77ebf745bcb1b644c97586576ab2751fa6fe
Dada878 <xx0932399@gmail.com>
2024-03-23 07:57:38 +0800

    add cache tag for posts api endpoint

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,15 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { Post } from "@/types/post";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const result = await fetch(
     "https://api.github.com/repos/dada878/dcard-homework/issues?q=state:open&sort=updated",
     {
       headers: {
         Authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
       },
+      next: {
+        tags: ["posts"],
+      },
     }
   );
+
   const data = await result.json();
   const posts: Array<Post> = [];
   for (const post of data) {
