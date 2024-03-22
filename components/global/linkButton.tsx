5eb6bdbf936399d2693237cb9a37c021bd2bf8c5
Dada878 <xx0932399@gmail.com>
2024-03-23 01:04:39 +0800

    create link button component

---
+++ components/global/linkButton.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Button from "./button";
+
+export default function LinkButton({
+  children,
+  color = "blue",
+  href,
+  className = "",
+  rounded = "rounded-xl"
+}: Readonly<{
+  children: React.ReactNode;
+  color? : "blue" | "red" | "green";
+  href: string;
+  className?: string;
+  rounded?: string;
+}>) {
+  return (
+    <Link href={href} className="flex">
+      <Button color={color} className={`${className} flex-1`} rounded={rounded} >
+        {children}
+      </Button>
+    </Link>
+  );
+}
