7a038f7502387ee4c8936555b2576cf7657d7d31
Dada878 <xx0932399@gmail.com>
2024-03-23 08:14:00 +0800

    add ContentRender component for rendering blog content

---
+++ components/blogView/contentRender.tsx
@@ -0,0 +1,28 @@
+import { Post } from "@/types/post";
+import TagItem from "../blogs/tagItem";
+import { formatDate } from "@/utils/dateFormatter";
+import Markdown from 'markdown-to-jsx';
+
+export default function ContentRender({ post }: { post: Post }) {
+  return (
+    <div className="dark:bg-mirage-900 bg-mirage-200 p-6 md:p-8 rounded-xl">
+      <h1 className="text-5xl font-bold">{post.title}</h1>
+      <div className="flex flex-row items-end justify-between py-4">
+        <div className="flex gap-2">
+          {post.tags.map((tag: string, i: number) => (
+            <TagItem key={i}>{tag}</TagItem>
+          ))}
+        </div>
+        {post && (
+          <p className="dark:text-secondary text-secondary-light text-sm">
+            {formatDate(new Date(post.date))}
+          </p>
+        )}
+      </div>
+      <hr className="text-secondary-dark py-3" />
+      <main className="markdown">
+        <Markdown>{post.content}</Markdown>
+      </main>
+    </div>
+  );
+}
