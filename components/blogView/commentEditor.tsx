f7ae93241e3ea691ad9f6507c0392a0eca8d0e00
Dada878 <xx0932399@gmail.com>
2024-03-23 08:10:12 +0800

    refactor comment editor component to use markdown-to-jsx

--- components/blogView/commentEditor.tsx
+++ components/blogView/commentEditor.tsx
@@ -1,30 +1,62 @@
 import { useState } from "react";
 import Button from "@/components/global/button";
 import Textarea from "@/components/global/textarea";
-import Markdown from "react-markdown";
+import Markdown from "markdown-to-jsx";
+
+function TabButton({
+  isPreview,
+  setIsPreview,
+  isToggleToPreview,
+}: {
+  isPreview: boolean;
+  setIsPreview: (isPreview: boolean) => void;
+  isToggleToPreview: boolean;
+}) {
+  return (
+    <p
+      onClick={() => {
+        setIsPreview(isToggleToPreview);
+      }}
+      className={`${
+        isPreview === isToggleToPreview
+          ? "dark:bg-mirage-900 bg-mirage-200"
+          : "dark:bg-mirage-800 bg-mirage-300 cursor-pointer"
+      } px-6 pt-3 pb-2 rounded-t-xl`}
+    >
+      {isToggleToPreview ? "預覽" : "撰寫"}
+    </p>
+  );
+}
 
 export default function CommentEditor() {
   const [isPreview, setIsPreview] = useState(false);
+  const [content, setContent] = useState("");
   return (
     <div className="flex flex-col rounded-xl">
       <div className="dark:bg-mirage-800 bg-mirage-300 flex justify-start w-full rounded-t-xl">
-        <p onClick={
-          () => {
-            setIsPreview(false);
-          }
-        } className={`${isPreview ? "dark:bg-mirage-800 bg-mirage-300 cursor-pointer" : "dark:bg-mirage-900 bg-mirage-200"} px-6 pt-3 pb-2 rounded-t-xl`}>撰寫</p>
-        <p onClick={
-          () => {
-            setIsPreview(true);
-          }
-        } className={`${isPreview ? "dark:bg-mirage-900 bg-mirage-200" : "dark:bg-mirage-800 bg-mirage-300 cursor-pointer"} px-6 pt-3 pb-2 transition rounded-t-xl`}>預覽</p>
+        <TabButton
+          isPreview={isPreview}
+          setIsPreview={setIsPreview}
+          isToggleToPreview={false}
+        />
+        <TabButton
+          isPreview={isPreview}
+          setIsPreview={setIsPreview}
+          isToggleToPreview={true}
+        />
       </div>
       <div className="dark:bg-mirage-900 bg-mirage-200 w-full p-4 rounded-b-xl flex gap-4 flex-col">
         <Textarea
           placeholder="在這裡輸入你想對這篇文章說的話..."
           className={`min-h-32 ${isPreview && "hidden"}`}
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
         />
-        <Markdown className={`markdown min-h-32 ${!isPreview && "hidden"}`}></Markdown>
+        <Markdown className={`markdown min-h-32 ${!isPreview && "hidden"}`}>
+          {content}
+        </Markdown>
         <Button className="w-full">送出留言</Button>
       </div>
     </div>
