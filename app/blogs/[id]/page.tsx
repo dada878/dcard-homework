503a7d33df8b9ba7c3284a5bc985c0d573221295
Dada878 <xx0932399@gmail.com>
2024-03-23 08:13:04 +0800

    refactor blog post page component

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,64 +1,39 @@
-"use client";
-import { useRouter } from "next/navigation";
-
 import MenuItem from "@/components/blogView/menuItem";
-import TagItem from "@/components/blogs/tagItem";
-import Button from "@/components/global/button";
 import Container from "@/components/global/container";
 import FixedSidebar from "@/components/global/fixedSidebar";
-import BlogComment from "@/components/blogView/comment";
-
-import Markdown from "react-markdown";
-import { faEdit, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import FloatingActionSection from "@/components/global/floatingActionSection";
-import CommentEditor from "@/components/blogView/commentEditor";
-import { useEffect, useState } from "react";
-import { Post } from "@/types/post";
-import { formatDate } from "@/utils/dateFormatter";
+import { Suspense } from "react";
 import { Comment } from "@/types/comment";
-import Dialog from "@/components/global/dialog";
+import LinkButton from "@/components/global/linkButton";
+import ContentRender from "@/components/blogView/contentRender";
+import FloatingActionSection from "@/components/global/floatingActionSection";
+import Markdown from "markdown-to-jsx";
+import BlogComment from "@/components/blogView/comment";
+import DeleteButtonClient from "./deleteButtonClient";
+import CommentEditorClient from "./commentEditorClient";
 
-export default function BlogPostPage({ params }: { params: { id: string } }) {
-  const router = useRouter();
-  const [post, setPost] = useState<Post>();
-  const [comments, setComments] = useState<Array<Comment>>([]);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  useEffect(() => {
-    fetch(`/api/posts/${params.id}`)
-      .then((result) => result.json())
-      .then((data) => {
-        setPost(data);
-      });
-    fetch(`/api/posts/${params.id}/comments`)
-      .then((result) => result.json())
-      .then((data) => {
-        setComments(data);
-      });
-  }, [params.id]);
+export default async function BlogPostPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const post = await fetch(`http://localhost:3000/api/posts/${params.id}`).then(
+    (result) => result.json()
+  );
+  const comments = await fetch(
+    `http://localhost:3000/api/posts/${params.id}/comments`
+  ).then((result) => result.json());
   return (
     <div className="flex">
       <FixedSidebar>
-        <Button
-          onClick={() => {
-            router.push(`/blogs/${params.id}/edit`);
-          }}
-        >
+        <LinkButton href={`/blogs/${params.id}/edit`}>
           <div className="flex gap-4 justify-center items-center">
             <FontAwesomeIcon className="w-4" icon={faEdit} />
             <span>編輯文章</span>
           </div>
-        </Button>
-        <Button
-          onClick={() => {
-            setIsDialogOpen(true);
-          }}
-        >
-          <div className="flex gap-4 justify-center items-center">
-            <FontAwesomeIcon className="w-4" icon={faTrashCan} />
-            <span>刪除文章</span>
-          </div>
-        </Button>
+        </LinkButton>
+        <DeleteButtonClient id={params.id} post={post} />
         <div className="dark:bg-mirage-900 bg-mirage-200 rounded-xl p-4">
           <h3 className="font-bold text-2xl text-center mb-4">文章目錄</h3>
           <div className="flex flex-col gap-2">
@@ -70,72 +45,30 @@ export default function BlogPostPage({ params }: { params: { id: string } }) {
         </div>
       </FixedSidebar>
       <Container width="w-full">
-        <div className="dark:bg-mirage-900 bg-mirage-200 p-6 md:p-8 rounded-xl">
-          <h1 className="text-5xl font-bold">{post?.title}</h1>
-          <div className="flex flex-row items-end justify-between py-4">
-            <div className="flex gap-2">
-              {post?.tags.map((tag, i) => (
-                <TagItem key={i}>{tag}</TagItem>
-              ))}
-            </div>
-            {post && (
-              <p className="dark:text-secondary text-secondary-light text-sm">
-                {formatDate(new Date(post.date))}
-              </p>
-            )}
-          </div>
-          <hr className="text-secondary-dark py-3" />
-          <main className="markdown">
-            <Markdown>{post?.content}</Markdown>
-          </main>
-        </div>
-        {comments.map((comment: Comment, i) => (
+        {post && <ContentRender post={post} />}
+        {comments.map((comment: Comment, i: number) => (
           <BlogComment
             key={i}
             userName={comment.author}
             avatarUrl={comment.avatar}
             date={new Date(comment.date)}
           >
+            <p>qwq</p>
             <Markdown>{comment.content}</Markdown>
           </BlogComment>
         ))}
-        <CommentEditor />
+        <Suspense fallback={<div>Loading...</div>}>
+          <CommentEditorClient />
+        </Suspense>
       </Container>
       <FloatingActionSection>
-        <Button
-          rounded="rounded-full"
-          onClick={() => {
-            router.push(`/blogs/${params.id}/edit`);
-          }}
-        >
+        <LinkButton href={`/blogs/${params.id}/edit`} rounded="rounded-full">
           <div className="p-2">
             <FontAwesomeIcon className="w-5 h-5 shadow-lg" icon={faEdit} />
           </div>
-        </Button>
-        <Button
-          rounded="rounded-full"
-          onClick={() => {
-            setIsDialogOpen(true);
-          }}
-        >
-          <div className="p-2">
-            <FontAwesomeIcon className="w-5 h-5 shadow-lg" icon={faTrashCan} />
-          </div>
-        </Button>
+        </LinkButton>
+        <DeleteButtonClient id={params.id} post={post} isMobile />
       </FloatingActionSection>
-      <Dialog
-        open={isDialogOpen}
-        setOpen={setIsDialogOpen}
-        onConfirm={() => {
-          fetch(`/api/posts/${params.id}/delete`, {
-            method: "POST",
-            body: JSON.stringify(post),
-          }).then(() => {
-            router.push("/blogs");
-          });
-        }}
-        title="確定要刪除文章嗎？"
-      />
     </div>
   );
 }
