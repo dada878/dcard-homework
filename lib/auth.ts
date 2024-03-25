a0966f5c6b6fcdfa26fb8465636c6c15401f0720
Dada878 <xx0932399@gmail.com>
2024-03-25 14:34:49 +0800

    update auth.ts to get id for a login user

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,16 +1,16 @@
-import { NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import type { Adapter } from 'next-auth/adapters';
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import type { Adapter } from "next-auth/adapters";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { cert } from "firebase-admin/app";
 
-export const authOptions : NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: FirestoreAdapter({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY!.replaceAll("\\n", "\n"),
-    })
+    }),
   }) as Adapter,
   secret: process.env.NEXTAUTH_SECRET,
   session: {
@@ -22,21 +22,26 @@ export const authOptions : NextAuthOptions = {
       clientSecret: process.env.GITHUB_APP_CLIENT_SECRET as string,
       authorization: {
         params: {
-          scope: "public_repo",
-        }
-      }
+          scope: "public_repo,user:email",
+        },
+      },
+      profile(profile) {
+        return profile;
+      },
     }),
   ],
   callbacks: {
     async session({ session, token, user }) {
       session.accessToken = token.accessToken as string;
+      session.user.id = token.userId as string;
       return session;
     },
     async jwt({ token, user, account, profile }) {
       if (account) {
+        token.userId = profile?.login;
         token.accessToken = account.access_token;
       }
       return token;
-    }
+    },
   },
 };
