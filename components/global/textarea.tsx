56bea6dde2393d4eb7ffbbdd6f846c21ff081299
Dada878 <xx0932399@gmail.com>
2024-03-25 18:49:39 +0800

    add disabled and name props to Textarea component

--- components/global/textarea.tsx
+++ components/global/textarea.tsx
@@ -3,15 +3,21 @@ export default function Textarea({
   onChange = () => {},
   placeholder = "",
   className = "",
+  disabled = false,
+  name = "",
 }: {
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   placeholder?: string;
   className?: string;
+  name?: string;
+  disabled?: boolean;
 }) {
   return (
     <textarea
       value={value}
+      name={name}
+      disabled={disabled}
       onChange={onChange}
       className={`${className} resize-none dark:bg-mirage-700 bg-mirage-300 p-3 w-full rounded-lg outline-none placeholder:text-secondary-light dark:placeholder:text-white/40`}
       placeholder={placeholder}
