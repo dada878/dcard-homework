bc9b9917af1be38d6f6a1eeb964cbdd27ad68505
Dada878 <xx0932399@gmail.com>
2024-03-25 18:50:21 +0800

    update getLoginUser function to return user id and add getLoginUserAvatar function

--- actions/auth.tsx
+++ actions/auth.tsx
@@ -5,7 +5,12 @@ import { getServerSession } from "next-auth";
 
 export async function getLoginUser() {
   const session = await getServerSession(authOptions);
-  return session?.user.name || undefined;
+  return session?.user.id || undefined;
+}
+
+export async function getLoginUserAvatar() {
+  const session = await getServerSession(authOptions);
+  return session?.user.image || undefined;
 }
 
 export async function isOwner() {
