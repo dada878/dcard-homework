6f011a71407c4586b5697002c5041e29a8d5eb6c
Dada878 <xx0932399@gmail.com>
2024-03-25 11:44:47 +0800

    use user's token to send request to avoid rate limit

--- actions/posts.tsx
+++ actions/posts.tsx
@@ -1,20 +1,30 @@
 "use server";
 
+import { authOptions } from "@/lib/auth";
 import { issueToPost } from "@/utils/issueToPost";
 import { parseLinkHeader } from "@/utils/linkHeaderParser";
 import { queryToURL } from "@/utils/queryToString";
+import { getServerSession } from "next-auth";
 
 const GITHUB_API_URL = `https://api.github.com/repos/${process.env.GITHUB_REPO_OWNER}/${process.env.GITHUB_REPO_NAME}/issues`;
-const GITHUB_HEADERS = {
-  Authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
-  Accept: "application/vnd.github+json",
-};
+
+async function sendRequest(url: string, body?: any, method: string = "GET") {
+  const session = await getServerSession(authOptions);
+  return fetch(`${GITHUB_API_URL}${url}`, {
+    method,
+    headers: {
+      Authorization: `Bearer ${
+        session?.accessToken || process.env.GITHUB_PERSONAL_ACCESS_TOKEN
+      }`,
+      Accept: "application/vnd.github+json",
+    },
+    body: JSON.stringify(body),
+  });
+}
 
 export async function getPostList(page: string = "1", query?: PostQuery) {
-  let queryString = query ? queryToURL(query, page) : "";
-  const result = await fetch(`${GITHUB_API_URL}?${queryString}`, {
-    headers: GITHUB_HEADERS,
-  });
+  const queryString = query ? queryToURL(query, page) : "";
+  const result = await sendRequest(`?${queryString}`);
   const data = await result.json();
   const posts = data.map((issue: Issue) => issueToPost(issue));
   return posts;
@@ -22,12 +32,7 @@ export async function getPostList(page: string = "1", query?: PostQuery) {
 
 export async function getPagination(page: string, query?: PostQuery) {
   const queryString = query ? queryToURL(query, page) : "";
-  const result = await fetch(
-    `${GITHUB_API_URL}?${queryString}`,
-    {
-      headers: GITHUB_HEADERS,
-    }
-  );
+  const result = await sendRequest(`?${queryString}`);
   const pagination = parseLinkHeader(result.headers.get("link")!);
   return {
     first: parseInt(pagination.first) || 1,
@@ -39,61 +44,45 @@ export async function getPagination(page: string, query?: PostQuery) {
 
 // TODO: here may can add a cache to reduce the request times
 export async function getPost(id: string) {
-  const result = await fetch(`${GITHUB_API_URL}/${id}`, {
-    headers: GITHUB_HEADERS,
-  });
+  const result = await sendRequest(`/${id}`);
   const issue: Issue = await result.json();
   return issueToPost(issue);
 }
 
 export async function createPost(post: Post) {
-  const result = await fetch(GITHUB_API_URL, {
-    method: "POST",
-    headers: GITHUB_HEADERS,
-    body: JSON.stringify({
-      title: post.title,
-      body: post.content,
-      labels: [
-        ...post.tags.map((tag) => `tag:${tag}`),
-        `category:${post.category}`,
-      ],
-    }),
-  });
+  const result = await sendRequest("", {
+    title: post.title,
+    body: post.content,
+    labels: [
+      ...post.tags.map((tag) => `tag:${tag}`),
+      `category:${post.category}`,
+    ],
+  }, "POST");
   const createdPost = await result.json();
   return createdPost.number;
 }
 
 export async function updatePost(id: string, post: Post) {
-  const result = await fetch(`${GITHUB_API_URL}/${id}`, {
-    method: "PATCH",
-    headers: GITHUB_HEADERS,
-    body: JSON.stringify({
-      title: post.title,
-      body: post.content,
-      labels: [
-        ...post.tags.map((tag) => `tag:${tag}`),
-        `category:${post.category}`,
-      ],
-    }),
-  });
+  const result = await sendRequest(`/${id}`, {
+    title: post.title,
+    body: post.content,
+    labels: [
+      ...post.tags.map((tag) => `tag:${tag}`),
+      `category:${post.category}`,
+    ],
+  }, "PATCH");
   return result.status;
 }
 
 export async function deletePost(id: string) {
-  const result = await fetch(`${GITHUB_API_URL}/${id}`, {
-    method: "PATCH",
-    headers: GITHUB_HEADERS,
-    body: JSON.stringify({
-      state: "closed",
-    }),
-  });
+  const result = await sendRequest(`/${id}`, {
+    state: "closed",
+  }, "PATCH");
   return result.status;
 }
 
 export async function getPostComments(id: string) {
-  const result = await fetch(`${GITHUB_API_URL}/${id}/comments`, {
-    headers: GITHUB_HEADERS,
-  });
+  const result = await sendRequest(`/${id}/comments`);
   const data = await result.json();
   const comments: Array<Comment> = data.map((comment: any) => {
     return {
