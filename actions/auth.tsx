663ad48cd47d87c255eca977664c5c1c2ec9679c
Dada878 <xx0932399@gmail.com>
2024-03-25 14:33:46 +0800

    update auth.tsx to return undefined for not login user in getLoginUser() function and check user id instead of email for isOwner()

--- actions/auth.tsx
+++ actions/auth.tsx
@@ -5,10 +5,10 @@ import { getServerSession } from "next-auth";
 
 export async function getLoginUser() {
   const session = await getServerSession(authOptions);
-  return session?.user.name || "Guest";
+  return session?.user.name || undefined;
 }
 
 export async function isOwner() {
   const session = await getServerSession(authOptions);
-  return session?.user.email === process.env.GITHUB_REPO_OWNER_EMAIL;
+  return session?.user.id === process.env.GITHUB_REPO_OWNER;
 }
