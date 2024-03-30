bba62793a4f3b987963097dd4934cedef133f675
Dada878 <xx0932399@gmail.com>
2024-03-31 00:39:50 +0800

    add max width to avoid content overflow

--- components/layout/container.tsx
+++ components/layout/container.tsx
@@ -11,7 +11,8 @@ export default function Container({
 }>) {
   return (
     <main
-      className={`flex flex-col gap-5 ml-0 md:ml-64 ${width} lg:ml-80 p-6 md:p-10 max-w-screen lg:max-w-[min(calc(100vw_-_19rem),56rem)] lg:w-4/6 ${className}`}
+      // NOTE: too complex
+      className={`flex flex-col gap-5 ml-0 md:ml-64 ${width} lg:ml-80 p-6 md:p-10 max-w-screen lg:max-w-[min(calc(100vw_-_19rem),56rem)] md:max-w-[min(calc(100vw_-_16rem),56rem)] lg:w-4/6 ${className}`}
     >
       {children}
     </main>
