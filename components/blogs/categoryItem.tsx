12ff2130bc92249801b94b8a91734eed88986b2f
Dada878 <xx0932399@gmail.com>
2024-03-31 01:34:06 +0800

    add hover classes in category item component

--- components/blogs/categoryItem.tsx
+++ components/blogs/categoryItem.tsx
@@ -14,14 +14,22 @@ export default function CategoryItem({
   const router = useRouter();
   const params = useSearchParams();
   const tags = params.get("tags")?.split(",") ?? [];
-  return <TagItem className="cursor-pointer hover:bg-mirage-500 dark:hover:bg-mirage-600 hover:text-black dark:hover:text-primary" selected={selected} onClick={() => {
-    const searchParams = new URLSearchParams();
-    searchParams.set("category", name);
-    // NOTE: maybe there is a better way check if the category is "all"
-    if (name === "全部") {
-      searchParams.delete("category");
-    }
-    searchParams.set("tags", tags.join(","));
-    router.push(`blogs?${searchParams.toString()}`);
-  }}>{name}</TagItem>;
+  return (
+    <TagItem
+      className="cursor-pointer hover:bg-mirage-500 dark:hover:bg-mirage-600 hover:text-black dark:hover:text-primary"
+      selected={selected}
+      onClick={() => {
+        const searchParams = new URLSearchParams();
+        searchParams.set("category", name);
+        // NOTE: maybe there is a better way check if the category is "all"
+        if (name === "全部") {
+          searchParams.delete("category");
+        }
+        searchParams.set("tags", tags.join(","));
+        router.push(`blogs?${searchParams.toString()}`);
+      }}
+    >
+      {name}
+    </TagItem>
+  );
 }
