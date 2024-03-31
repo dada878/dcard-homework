3dd49b9601a895e9997f8d7c16732934dd9a0882
Dada878 <xx0932399@gmail.com>
2024-03-31 22:38:40 +0800

    add some class to prevent text in tag item component wrap

--- components/global/tagItem.tsx
+++ components/global/tagItem.tsx
@@ -14,7 +14,7 @@ export default function TagItem({
   return (
     <button
       onClick={onClick}
-      className={`dark:bg-mirage-700 line-clamp-1 bg-mirage-400 py-1 px-2 text-secondary-light dark:text-secondary rounded-md cursor-pointer transition text-center ${className ?? ""} ${
+      className={`dark:bg-mirage-700 line-clamp-1 text-nowrap bg-mirage-400 py-1 px-2 text-secondary-light dark:text-secondary rounded-md cursor-pointer transition text-center ${className ?? ""} ${
         selected ? "outline outline-1 outline-mirage-500" : ""
       }`}
     >
