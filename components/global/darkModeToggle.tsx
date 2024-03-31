621b8cae26795fbff1d8fb0a6a14b63f9b987d81
Dada878 <xx0932399@gmail.com>
2024-04-01 00:49:12 +0800

    fix document not found error

--- components/global/darkModeToggle.tsx
+++ components/global/darkModeToggle.tsx
@@ -1,44 +1,48 @@
+import { useEffect } from "react";
+
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Switch from "react-switch";
-import useLocalStorageState from 'use-local-storage-state';
+import useLocalStorageState from "use-local-storage-state";
 
 export default function DarkModeToggle() {
   const [theme, setTheme] = useLocalStorageState("theme");
 
-  if (theme === "dark") {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
 
   return (
     <div className="hidden md:block">
       <Switch
-      className="scale-125"
-      onChange={(checked) => {
-        setTheme(checked ? "dark" : "light");
-      }}
-      checked={theme === "dark"}
-      onColor="#51586d"
-      offColor="#f4cd8c"
-      offHandleColor="#eea950"
-      onHandleColor="#6c7693"
-      boxShadow="0px 0px 0px 0px #000000"
-      activeBoxShadow="0px 0px 0px 0px #000000"
-      uncheckedHandleIcon={
-        <div className="flex justify-center items-center h-full text-white">
-          <FontAwesomeIcon icon={faSun} />
-        </div>
-      }
-      checkedHandleIcon={
-        <div className="flex justify-center items-center h-full">
-          <FontAwesomeIcon icon={faMoon} />
-        </div>
-      }
-      checkedIcon={<div></div>}
-      uncheckedIcon={<div></div>}
-    />
+        className="scale-125"
+        onChange={(checked) => {
+          setTheme(checked ? "light" : "dark");
+        }}
+        checked={theme === "light"}
+        offColor="#51586d"
+        onColor="#f4cd8c"
+        onHandleColor="#eea950"
+        offHandleColor="#6c7693"
+        boxShadow="0px 0px 0px 0px #000000"
+        activeBoxShadow="0px 0px 0px 0px #000000"
+        checkedHandleIcon={
+          <div className="flex justify-center items-center h-full text-white">
+            <FontAwesomeIcon icon={faSun} />
+          </div>
+        }
+        uncheckedHandleIcon={
+          <div className="flex justify-center items-center h-full">
+            <FontAwesomeIcon icon={faMoon} />
+          </div>
+        }
+        checkedIcon={<div></div>}
+        uncheckedIcon={<div></div>}
+      />
     </div>
   );
 }
