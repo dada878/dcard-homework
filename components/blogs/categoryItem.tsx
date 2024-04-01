14bc68bbe245f832c7b63558427407f66d5c7a89
Dada878 <xx0932399@gmail.com>
2024-04-01 14:54:06 +0800

    refactor useQueryFilter to return new url instead instead of push route immediate

--- components/blogs/categoryItem.tsx
+++ components/blogs/categoryItem.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 import useQueryFilter from "@/hooks/useQueryFilter";
 
 import TagItem from "../global/tagItem";
@@ -12,15 +14,15 @@ export default function CategoryItem({
   selected?: boolean;
 }>) {
   const { setCategory } = useQueryFilter();
+  const newUrl = setCategory(name);
   return (
-    <TagItem
-      className="cursor-pointer hover:bg-mirage-500 dark:hover:bg-mirage-600 hover:text-black dark:hover:text-primary"
-      selected={selected}
-      onClick={() => {
-        setCategory(name);
-      }}
-    >
-      {name}
-    </TagItem>
+    <Link href={newUrl}>
+      <TagItem
+        className="cursor-pointer w-full hover:bg-mirage-500 dark:hover:bg-mirage-600 hover:text-black dark:hover:text-primary"
+        selected={selected}
+      >
+        {name}
+      </TagItem>
+    </Link>
   );
 }
--- components/blogs/togglableTagItem.tsx
+++ components/blogs/togglableTagItem.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 import useQueryFilter from "@/hooks/useQueryFilter";
 
 import TagItem from "../global/tagItem";
@@ -12,20 +14,18 @@ export default function TogglableTagItem({
   name: string;
   selected?: boolean;
 }>) {
-  const {tags, setTags} = useQueryFilter();
+  const { tags, setTags } = useQueryFilter();
+  const newUrl = setTags(
+    selected ? tags.filter((tag) => tag !== name) : [...tags, name]
+  );
   return (
-    <TagItem
-      selected={selected}
-      className="cursor-pointer hover:bg-mirage-500 dark:hover:bg-mirage-600 hover:text-black dark:hover:text-primary"
-      onClick={() => {
-        if (selected) {
-          setTags(tags.filter((tag) => tag !== name));
-        } else {
-          setTags([...tags, name]);
-        }
-      }}
-    >
-      {name}
-    </TagItem>
+    <Link href={newUrl}>
+      <TagItem
+        selected={selected}
+        className="cursor-pointer hover:bg-mirage-500 dark:hover:bg-mirage-600 hover:text-black dark:hover:text-primary"
+      >
+        {name}
+      </TagItem>
+    </Link>
   );
 }
