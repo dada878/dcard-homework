684ad2296d267226635d7ca0d62130fbc245d081
Dada878 <xx0932399@gmail.com>
2024-04-06 07:28:12 +0800

    add a simple caching to fetchGithubAPI function

--- utils/fetchGithubAPI.ts
+++ utils/fetchGithubAPI.ts
@@ -5,12 +5,19 @@ import { authOptions } from "@/lib/auth";
 
 const GITHUB_API_URL = `https://api.github.com/repos/${process.env.GITHUB_REPO_OWNER}/${process.env.GITHUB_REPO_NAME}`;
 
+const cache = new Map<string, any>();
+
 export async function fetchGithubAPI(
   url: string,
   body?: any,
   method: string = "GET",
 ) {
   const session = await getServerSession(authOptions);
+  // cache all when not authenticated
+  if (cache.has(url) && method === "GET" && !session) {
+    return cache.get(url);
+  }
+
   const result = await fetch(`${GITHUB_API_URL}${url}`, {
     method,
     headers: {
@@ -21,9 +28,13 @@ export async function fetchGithubAPI(
     },
     body: JSON.stringify(body),
   });
+
   const data = await result.json();
   if (result.status >= 400) {
     throw new Error(data.message);
   }
+
+  cache.set(url, data);
+  
   return data;
 }
