e1a2f56cbee85300ff875a1ec41960e3c7d57f1e
Dada878 <xx0932399@gmail.com>
2024-04-05 13:28:36 +0800

    update dark mode toggle style

--- components/navbar/darkModeToggle.tsx
+++ components/navbar/darkModeToggle.tsx
@@ -10,28 +10,26 @@ export default function DarkModeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="hidden md:block">
-      <div
-        className="relative h-10 w-20 rounded-full bg-[#f4ce8c74] dark:bg-[#51586d5f]"
-        onClick={() => {
-          setTheme(theme === "light" ? "dark" : "light");
-        }}
+    <div
+      className="relative hidden h-10 w-20 cursor-pointer rounded-full bg-[#f4ce8c74] md:block dark:bg-[#51586d5f]"
+      onClick={() => {
+        setTheme(theme === "light" ? "dark" : "light");
+      }}
+    >
+      <span
+        className={cn(
+          "absolute bottom-1/2 left-0 size-10 translate-y-1/2 rounded-full bg-[#f4cd8c] transition-all dark:left-10 dark:bg-[#51586d]",
+        )}
       >
-        <span
-          className={cn(
-            "absolute bottom-1/2 left-0 size-10 translate-y-1/2 rounded-full bg-[#f4cd8c] transition-all dark:left-10 dark:bg-[#51586d]",
-          )}
-        >
-          <FontAwesomeIcon
-            icon={faSun}
-            className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[#eea950] dark:hidden "
-          />
-          <FontAwesomeIcon
-            icon={faMoon}
-            className="absolute left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 dark:block"
-          />
-        </span>
-      </div>
+        <FontAwesomeIcon
+          icon={faSun}
+          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[#eea950] dark:hidden "
+        />
+        <FontAwesomeIcon
+          icon={faMoon}
+          className="absolute left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 text-white dark:block"
+        />
+      </span>
     </div>
   );
 }
