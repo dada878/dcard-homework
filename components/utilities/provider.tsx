192d4cbdd16b3001f5333e5fbdaa7a1b163e93ba
Dada878 <xx0932399@gmail.com>
2024-04-05 17:30:31 +0800

    add some cool console log message

--- components/utilities/provider.tsx
+++ components/utilities/provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import "@/utils/consoleLog";
 
 export default function Provider({
   children,
---
+++ utils/consoleLog.ts
@@ -0,0 +1,3 @@
+console.log("%c在非洲部落中，傳說拔到獅子的鬃毛，掉落的頭髮就會長回來……別再相信沒有根據的說法了", "color: red; font-size:24px;");
+console.log("%c如果有人叫你在這裡複製貼上那絕對是在騙你 ¯\\_(ツ)_/¯", "color: red; font-size:24px;");
+console.log("%c如果你知道你在幹嘛，歡迎錄取我 \\(.D˙)/ ", "font-size:18px;");
