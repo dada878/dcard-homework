b6f3bbd06df2d06b18e2d77dca41f7d666c4cb99
Dada878 <xx0932399@gmail.com>
2024-04-06 17:34:16 +0800

    fix error: sometimes the dark mode toggle displays the wrong icon

--- components/navbar/darkModeToggle.tsx
+++ components/navbar/darkModeToggle.tsx
@@ -11,7 +11,7 @@ export default function DarkModeToggle() {
 
   return (
     <div
-      className="relative hidden h-10 w-20 cursor-pointer rounded-full bg-[#f4ce8c74] md:block dark:bg-[#51586d5f]"
+      className="relative hidden h-10 w-20 cursor-pointer rounded-full bg-[#f4ce8c74] dark:bg-[#51586d5f] md:block"
       onClick={() => {
         setTheme(theme === "light" ? "dark" : "light");
       }}
@@ -23,11 +23,11 @@ export default function DarkModeToggle() {
       >
         <FontAwesomeIcon
           icon={faSun}
-          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[#eea950] dark:hidden "
+          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[#eea950] opacity-100 dark:opacity-0"
         />
         <FontAwesomeIcon
           icon={faMoon}
-          className="absolute left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 text-white dark:block"
+          className="absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 dark:opacity-100"
         />
       </span>
     </div>
