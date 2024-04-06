d23901ac1e7b1a7f1d5a031dc606e54d698bc67f
Dada878 <xx0932399@gmail.com>
2024-04-06 14:49:36 +0800

    use another way to avoid yaml parsing error

--- utils/postToIssue.ts
+++ utils/postToIssue.ts
@@ -2,9 +2,9 @@ export function postToIssue(post: Post): Issue {
   return {
     title: post.title,
     // avoid yaml parsing error
-    body: `---\ndescription: ${post.description
+    body: `---\ndescription: "${post.description
       .replaceAll("\n", " ")
-      .replaceAll(":", "：")}\n---\n${post.content}`,
+      .replaceAll(`"`, `\\"`)}"\n---\n${post.content}`,
     labels: [
       ...post.tags.map((tag) => ({ name: `tag:${tag}` })),
       { name: `category:${post.category}` },
