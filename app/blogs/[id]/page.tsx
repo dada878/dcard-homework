514f583d63a55dcf991694865340607114d1966d
Dada878 <xx0932399@gmail.com>
2024-04-23 12:14:08 +0800

    implement lazy loading for blog article content

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { Suspense } from "react";
+
 import { notFound } from "next/navigation";
+import Skeleton from "react-loading-skeleton";
 
 import { isAdmin } from "@/actions/auth";
 import { getPost, getPostComments } from "@/actions/posts";
@@ -7,7 +10,9 @@ import BlogPageFloatingActions from "@/components/blog/blogPageFloatingActions";
 import BlogPageSidebar from "@/components/blog/blogPageSidebar";
 import CommentSection from "@/components/comments/commentSection";
 import Container from "@/components/layout/container";
+import Card from "@/components/utilities/card";
 import { defaultSEO } from "@/utils/seo";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export async function generateMetadata({
   params,
@@ -32,18 +37,47 @@ export async function generateMetadata({
   return metadata;
 }
 
+async function MainContentSkeleton() {
+  const baseColor = "#4b5c90";
+  const highlightColor = "#778db6";
+  const colorProps = { baseColor, highlightColor };
+  return (
+    <Container className="w-full">
+      <Card className="flex flex-col gap-3 p-8">
+        <Skeleton count={1} height={50} {...colorProps} className="mb-2" />
+        <div className="flex gap-2">
+          <Skeleton count={1} width={100} height={30} {...colorProps} />
+          <Skeleton count={1} width={100} height={30} {...colorProps} />
+        </div>
+        <Skeleton count={7} enableAnimation={true} {...colorProps} />
+        <Skeleton count={5} enableAnimation={true} {...colorProps} />
+      </Card>
+    </Container>
+  );
+}
+
+async function MainContent({ id }: Readonly<{ id: string }>) {
+  const [post, comments] = await Promise.all([
+    getPost(id),
+    getPostComments(id),
+  ]).catch(() => {
+    notFound();
+  });
+
+  return (
+    <Container className="w-full">
+      {post && <BlogContent post={post} />}
+      <CommentSection comments={comments} postId={id} />
+    </Container>
+  );
+}
+
 export default async function BlogPostPage({
   params,
 }: Readonly<{
   params: { id: string };
 }>) {
-  const [post, comments, showActionButtons] = await Promise.all([
-    getPost(params.id),
-    getPostComments(params.id),
-    isAdmin(),
-  ]).catch(() => {
-    notFound();
-  });
+  const showActionButtons = await isAdmin();
 
   return (
     <div className="flex">
@@ -52,10 +86,9 @@ export default async function BlogPostPage({
         postID={params.id}
       />
 
-      <Container className="w-full">
-        {post && <BlogContent post={post} />}
-        <CommentSection comments={comments} postId={params.id} />
-      </Container>
+      <Suspense fallback={<MainContentSkeleton />}>
+        <MainContent id={params.id} />
+      </Suspense>
 
       <BlogPageFloatingActions
         showActionButtons={showActionButtons}
